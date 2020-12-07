7f9be2b39e024eeb41ef3c7058a846fa1d573104
Harut <harutghandilyan@gmail.com>
2020-12-07 16:09:28 +0400

    test branch

--- .github/scripts/slack_send_message.sh
+++ .github/scripts/slack_send_message.sh
@@ -1,4 +1,4 @@
 curl -L -X POST 'https://slack.com/api/chat.postMessage' -H "Authorization: Bearer $slack_app_token" -H 'Content-Type: application/json' --data-raw '{
   "channel": "C01G0V89Z7T",
-  "text": "Hello taza ticket :tada:"
+  "text": "pullrequest works with comment"
 }'
