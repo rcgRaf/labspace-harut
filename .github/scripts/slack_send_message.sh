fe115a0ed7b036bf75e1454f1b79d6dd9c0f84f0
Harut <harutghandilyan@gmail.com>
2020-12-08 13:05:57 +0400

    asdfasdf

--- .github/scripts/slack_send_message.sh
+++ .github/scripts/slack_send_message.sh
@@ -1,4 +1,6 @@
-curl -L -X POST 'https://slack.com/api/chat.postMessage' -H "Authorization: Bearer $slack_app_token" -H 'Content-Type: application/json' --data-raw '{
-  "channel": "C01G0V89Z7T",
-  "text": "'$PR_json'"
-}'
+curl -L -X POST 'https://slack.com/api/chat.postMessage' -H "Authorization: Bearer $slack_app_token" -H 'Content-Type: application/json' --data-raw "{
+  'channel': 'C01G0V89Z7T',
+  'text': '$PR_json'
+}"
+
+
