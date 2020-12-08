6ce0313835c721456db0de388c33b8287404a845
Harut <harutghandilyan@gmail.com>
2020-12-08 12:31:09 +0400

    asdgfasdfsfa

--- .github/scripts/get_pr_as_json.sh
+++ .github/scripts/get_pr_as_json.sh
@@ -1,6 +1,6 @@
 export PR_json=$(curl \
   -H "Authorization: token $git_token" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/thekord92/labspace-harut/pulls/$pr_id | jq --raw-output '.AssetID') 
+  https://api.github.com/repos/thekord92/labspace-harut/pulls/$pr_id)
 
-echo "$PR_json"
+echo $PR_json
