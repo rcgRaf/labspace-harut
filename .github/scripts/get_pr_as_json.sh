db00a3afa6c38a814e18bec3700b1d3693c32fe2
Harut <harutghandilyan@gmail.com>
2020-12-08 12:01:27 +0400

    hamarya

--- .github/scripts/get_pr_as_json.sh
+++ .github/scripts/get_pr_as_json.sh
@@ -1,4 +1,6 @@
-curl \
+PR_json = $(curl \
   -H "Authorization: token $git_token" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/thekord92/labspace-harut/pulls/$pr_id
+  https://api.github.com/repos/thekord92/labspace-harut/pulls/$pr_id)
+
+echo "$PR_json"
